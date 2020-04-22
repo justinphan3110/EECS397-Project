c8c982373e3ed8391d53f7be1755c39080c55b27
long <justinphan3110@gmail.com>
2020-04-22 13:38:52 -0400

    hot fix

--- project2/api/api.js
+++ project2/api/api.js
@@ -19,7 +19,7 @@ app.get("/:company", (req, res) => {
   client
     .query(req.params.company.toUpperCase())
     .then((data) => {
-        console.log("Data: " + data);
+        console.log("Data: " + data.results);
         if(data.results !== undefined){
             res.json((data.results[0].series[0].values));
         }
