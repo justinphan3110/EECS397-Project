81812f603e4af898f8d8e3636d9326296d4e3d32
long <justinphan3110@gmail.com>
2020-04-22 12:21:45 -0400

    adding project2 db and api

---
+++ project2/api/api.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(cors());
+
+// Listtening on PORT 3001
+app.listen(3001);
+
+// Connect to Influx db
+const Influx = require("influxdb-nodejs");
+const client = new Influx("http://127.0.0.1:8086/stock");
+
+app.get("/:company", (req, res) => {
+  console.log("calling query: "+ req.params.company.toUpperCase());
+  client
+    .query(req.params.company.toUpperCase())
+    .then((data) => {
+        res.json((data.results[0].series[0].values));
+    })
+    .catch(console.error);
+});
