302713810cebd6fd2e20ad886d45686665ea00ae
justinphan3110 <justinphan3110@gmail.com>
2020-04-27 19:50:01 -0400

    time - now - 300m

--- project2/api/api.js
+++ project2/api/api.js
@@ -22,8 +22,7 @@ app.get("/test", (req, res) => {
 app.get("/:company", (req, res) => {
   console.log("calling query: "+ req.params.company.toUpperCase());
   client
-    .query("SELECT * from req.params.company.toUpperCase().split('=')[1]")
-    // .where('time', 'now() - 30m', '>')
+    .query(req.params.company.toUpperCase().split('=')[1])
     .then((data) => {
         console.log("Data: " + data.results.series);
         if(data.results[0].series !== undefined){
